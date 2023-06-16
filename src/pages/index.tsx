19ced830ab4ac32f1c01bb007d8b1d5bd978673c
mykaeull <mykaeull99@gmail.com>
2023-06-16 11:54:23 -0300

    config: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Portifólio | Mykaeull Gomes</title>
+                <meta
+                    name="description"
+                    content="Mykaeull Gomes is a web developer..."
+                />
+            </Head>
+            <div>
+                <h1>Home</h1>
+            </div>
+        </>
+    );
+}
